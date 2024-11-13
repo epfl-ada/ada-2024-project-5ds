7f2d8f7586ed10f4eb7f9488a0da327d14192d78
Wfiles <william.jallot@epfl.ch>
2024-11-13 15:44:04 +0100

    load reviews but error

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -6,6 +6,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 from utils.csv import save_dataframe_to_csv
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def load_initial_dataset(PATH_IN) :
 
@@ -48,8 +52,32 @@ def load_oscar_winning_films(OS = 'WINDOWS') :
     df = search_golden_names(url,OS)
     save_dataframe_to_csv(df, "oscar_winning_films")
 
+
+
 def load_oscar_winning_actors(OS = 'WINDOWS') :
-    
+    """
+    Search all the actors that won an oscar 
+    Params :
+        OS : The opearting system you are own, default Windows
+    """
+    url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Actor"
+    names_list = search_special_symbols(url, OS)
+    df = pd.DataFrame(names_list, columns=['actors'])
+    save_dataframe_to_csv(df, 'oscar_winning_actors')
+
+def load_oscar_winning_actresses(OS = 'WINDOWS') :
+    """
+    Search all the actresses that won an oscar 
+    Params :
+        OS : The opearting system you are own, default Windows
+    """
+    url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Actress"
+    names_list = search_special_symbols(url, OS)
+    df = pd.DataFrame(names_list, columns=['actresses'])
+    save_dataframe_to_csv(df, 'oscar_winning_actresses')
+   
+def load_reviews(movies_list, max_number_of_reviews,OS) :
+
     if OS == 'MAC' :
         driver = webdriver.Chrome()
     else :
@@ -57,33 +85,88 @@ def load_oscar_winning_actors(OS = 'WINDOWS') :
         options = webdriver.ChromeOptions()
         driver = webdriver.Chrome(service = service, options = options)
 
-    names_list = []
-   
-    url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Actor"
-    driver.get(url)
+    real_movie_titles = []
+    movie_reviews = pd.DataFrame(columns=['Movie name', 'Review'])
 
-    # Adding a wait to ensure all content is loaded
+    for movie_title in movies_list:
+        print(movie_title[0])
+        url = f"https://www.rottentomatoes.com/search?search={movie_title[0].replace(' ', '_').lower()}"
+        driver.get(url)
+        time.sleep(5)
+        link = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "//a[@data-qa='info-name']"))  # Replace with your actual locator
+        )
+        link.click()
+        time.sleep(5)
+        # Wait until the rt-text element with the slot attribute is present
+        rt_text_element = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "//rt-text[@slot='title']"))
+        )
+
+
+        title_name = rt_text_element.text
+        real_movie_titles.append(title_name)
+        base_url = driver.current_url
+        
+        reviews_url = f"{base_url}/reviews?type=user"
+        
+
+        driver.get(reviews_url)
+    
+        time.sleep(1)  
+       
+        number_reviews = 0
+        output_txt_path = 'movie_reviews.txt'
+
+        with open(output_txt_path, mode='a', encoding='utf-8') as file:
+            while number_reviews < max_number_of_reviews:
+                try:
+                    reviews = WebDriverWait(driver, 15).until(
+                        EC.presence_of_all_elements_located((By.XPATH, "//p[@class='audience-reviews__review js-review-text']"))
+                    )
+                    for review in reviews:
+                        review_text = review.text.strip()
+                        movie_review_entry = f"Movie name: {title_name}\nReview: {review_text}\n\n"
+                        file.write(movie_review_entry)
+                        number_reviews += 1
+
+                    load_more_button = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.XPATH, "//rt-button[@data-qa='load-more-btn']"))
+                    )
+                    driver.execute_script("arguments[0].scrollIntoView(true);", load_more_button)
+                    load_more_button.click()
+                except:
+                    break
+    driver.quit()
+    return real_movie_titles, 
+
+
+def search_special_symbols(url, OS) : 
+    if OS == 'MAC' :
+        driver = webdriver.Chrome()
+    else :
+        service = Service('C:\webdrivers\chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service = service, options = options)
+    driver.get(url)
+    names_list = []
     time.sleep(5)
 
-    # Find all 'tr' tags in the table to extract year, movie, and symbol information
     tr_tags = driver.find_elements(By.TAG_NAME, 'tr')
 
-    # Iterate over each 'tr' tag to find the required information
     for tr_tag in tr_tags:
         # Extract 'td' elements from the row
         td_tags = tr_tag.find_elements(By.TAG_NAME, 'td')
 
         if len(td_tags) > 0:
-            # Extract the text content and background color from each 'td' tag
             for td_tag in td_tags:
                 text = td_tag.text.strip()
-                style = td_tag.get_attribute('style')
 
-                # Check for specific symbols in the text and categorize them
                 if '‡' in text:
                     cleaned_text = text.replace('‡', '').strip()
                     first_two_words = ' '.join(cleaned_text.split()[:2])
                     names_list.append(first_two_words)
+
                 #Refused oscar
                 elif '§' in text:
                     cleaned_text = text.replace('§', '').strip()
@@ -93,9 +176,7 @@ def load_oscar_winning_actors(OS = 'WINDOWS') :
                     cleaned_text = text.replace('†', '').strip()
                 
     driver.quit()
-    df = pd.DataFrame(names_list, columns=['actors'])
-    save_dataframe_to_csv(df, 'oscar_winning_actors')
-   
+    return names_list
 
 def search_golden_names(url, OS) :
     if OS == 'MAC' :
