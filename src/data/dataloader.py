d35e2186fde4e6bba40856bbb69a4f8b0094f477
Wfiles <william.jallot@epfl.ch>
2024-11-14 10:57:25 +0100

    Commented the code, and scrapped the film ID along  with the winning actors

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -14,24 +14,41 @@ import requests
 
 
 def load_initial_dataset(PATH_IN) :
+    """
+    Load the CMU dataset
+    Params :
+        PATH_IN : The file where are located the CMU data
+    Return :
+        movie : A panda dataframe containing movies with the following columns : 'Wikipedia movie ID', 'Freebase movie ID',
+                'Movie name', 'Movie release date', 'Movie box office revenue', 'Movie runtime', 'Movie languages',
+                'Movie countries', 'Movie genres'
+        character : A panda dataframe containing movie characters with the following columns : 'Wikipedia movie ID', 'Freebase movie ID', 'Movie release date', 'Character name', 'Actor date of birth', ' Actor gender', 'Actor height)', 'Actor ethnicity', 'Actor name',
+                        'Actor age at movie release', 'Freebase character', 'Freebase character ID', 'Freebase actor ID'
+        plot summaries : A panda dataframe containing movie plot summaries containing the following columns : 'Wikipedia movie ID', 'Summary'
 
+        tv tropes : A panda dataframe containing tv tropes with the following columns : 'Trope', 'Character Name', 'Movie name', 'Freebase movie ID', 'Actor name'
+
+        name clusters : A panda dataframe containing characters names from film with the freebase ID with the following columns : 'Character Name', 'ID'
+    """
+    #Load the movie dataframe
     fname = os.path.join(PATH_IN, 'movie.metadata.tsv')
     movie = pd.read_csv(fname, delimiter='\t', header=None)
-    movie.columns = ['Wikipedia movie ID', 'Freebase movie ID', 'Movie name', 'Movie release date', 'Movie box office revenue', 'Movie runtime', 'Movie languages (Freebase ID:name tuples)', 'Movie countries (Freebase ID:name tuples)', 'Movie genres (Freebase ID:name tuples)']
+    movie.columns = ['Wikipedia movie ID', 'Freebase movie ID', 'Movie name', 'Movie release date', 'Movie box office revenue', 'Movie runtime', 'Movie languages', 'Movie countries', 'Movie genres']
 
+    #load the character dataframe
     fname = os.path.join(PATH_IN, 'character.metadata.tsv')
     character = pd.read_csv(fname, delimiter='\t', header=None)
-    character.columns = ['Wikipedia movie ID', 'Freebase movie ID', 'Movie release date', 'Character name', 'Actor date of birth', ' Actor gender', 'Actor height (in meters)', 'Actor ethnicity (Freebase ID)', 'Actor name',
-                        'Actor age at movie release', 'Freebase character/actor map ID', 'Freebase character ID', 'Freebase actor ID']
+    character.columns = ['Wikipedia movie ID', 'Freebase movie ID', 'Movie release date', 'Character name', 'Actor date of birth', ' Actor gender', 'Actor height)', 'Actor ethnicity', 'Actor name',
+                        'Actor age at movie release', 'Freebase character', 'Freebase character ID', 'Freebase actor ID']
 
+    #Load the plot summary data frame
     fname = os.path.join(PATH_IN, 'plot_summaries.txt')
     plot_summaries = pd.read_csv(fname, delimiter='\t', header=None)
     plot_summaries.columns = ['Wikipedia movie ID', 'Summary']
 
-
+    #Load the tv tropes dataframe
     fname = os.path.join(PATH_IN, 'tvtropes.clusters.txt')
     tvtropes = pd.read_csv(fname, delimiter='\t', header=None, names = ['Trope','StringDict'])
-
 
     tvtropes['Dictionnary'] = tvtropes['StringDict'].apply(ast.literal_eval)
     df = pd.json_normalize(tvtropes['Dictionnary'])
@@ -39,68 +56,190 @@ def load_initial_dataset(PATH_IN) :
     tvtropes['Movie name'] = df['movie']
     tvtropes['Freebase movie ID'] = df['id']
     tvtropes['Actor name'] = df['actor']
+    #Drop the duplicated columns 
     tvtropes = tvtropes.drop(tvtropes.columns[1], axis=1)
     tvtropes = tvtropes.drop(tvtropes.columns[1], axis=1)
 
+    #Load the name_cluster datframe
     fname = os.path.join(PATH_IN, 'name.clusters.txt')
     name_clusters = pd.read_csv(fname, delimiter='\t', header=None, names = ['Character Name','ID'])
 
     return  movie, character, plot_summaries,  tvtropes, name_clusters
 
-def load_oscar_winning_films(OS = 'WINDOWS') :
-    url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Picture"
-    df = search_golden_names(url,OS)
-    save_dataframe_to_csv(df, "oscar_winning_films")
 
 def load_oscar_winning_films_ids(OS = 'WINDOWS') :
+    """
+    Search all the films that won an oscar and save it with the wikidata id in the cleaned_data directory
+    Params :
+        OS : The opearting system you are own, default Windows
+    """
     url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Picture"
-    df = search_golden_names_ids(url,OS)
+    page_ids = search_winning_films_ids(url,OS)
+    df= pd.DataFrame(page_ids, columns=['Page ID'])
     save_dataframe_to_csv(df, "oscar_winning_films_ids")
 
 def load_oscar_winning_actors(OS = 'WINDOWS') :
     """
-    Search all the actors that won an oscar 
+    Search all the actors that won an oscar and save the actor along with the wikipedia film id in the cleane_data directory
     Params :
         OS : The opearting system you are own, default Windows
     """
     url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Actor"
-    names_list = search_special_symbols(url, OS)
-    df = pd.DataFrame(names_list, columns=['actors'])
+    page_ids = search_actors_and_films(url, OS)
+    df = pd.DataFrame([(name, id_) for name, ids in page_ids.items() for id_ in ids], columns=['Actor', 'film_id'])
     save_dataframe_to_csv(df, 'oscar_winning_actors')
 
 def load_oscar_winning_actresses(OS = 'WINDOWS') :
     """
-    Search all the actresses that won an oscar 
+    Search all the actresses that won an oscar and save the actor along with the wikipedia film id in the cleane_data directory
     Params :
         OS : The opearting system you are own, default Windows
     """
     url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Actress"
-    names_list = search_special_symbols(url, OS)
-    df = pd.DataFrame(names_list, columns=['actresses'])
+    page_ids = search_actors_and_films(url, OS)
+    df = pd.DataFrame([(name, id_) for name, ids in page_ids.items() for id_ in ids], columns=['Actress', 'film_id'])
     save_dataframe_to_csv(df, 'oscar_winning_actresses')
 
 def load_oscar_winning_supporting_actors(OS = 'WINDOWS') :
     """
-    Search all the supporting actors that won an oscar 
+    Search all the supporting actors that won an oscar and save the actor along with the wikipedia film id in the cleane_data directory
     Params :
         OS : The opearting system you are own, default Windows
     """
     url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Supporting_Actor"
-    names_list = search_special_symbols(url, OS)
-    df = pd.DataFrame(names_list, columns=['supporting_actors'])
+    page_ids = search_actors_and_films(url, OS)
+    df = pd.DataFrame([(name, id_) for name, ids in page_ids.items() for id_ in ids], columns=['Supporting Actor', 'film_id'])
     save_dataframe_to_csv(df, 'oscar_winning_supporting_actors')
+
+    
 
 def load_oscar_winning_supporting_actresses(OS = 'WINDOWS') :
     """
-    Search all the supporting actresses that won an oscar 
+    Search all the supporting actresses that won an oscar and save the actor along with the wikipedia film id in the cleane_data directory
     Params :
         OS : The opearting system you are own, default Windows
     """
     url = "https://en.wikipedia.org/wiki/Academy_Award_for_Best_Supporting_Actress"
-    names_list = search_special_symbols(url, OS)
-    df = pd.DataFrame(names_list, columns=['supporting_actresses'])
+    page_ids = search_actors_and_films(url, OS)
+    df = pd.DataFrame([(name, id_) for name, ids in page_ids.items() for id_ in ids], columns=['Supporting Actress', 'film_id'])
     save_dataframe_to_csv(df, 'oscar_winning_supporting_actresses')
    
+
+def search_actors_and_films(url, OS) : 
+    if OS == 'MAC' :
+        driver = webdriver.Chrome()
+    else :
+        service = Service('C:\webdrivers\chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service = service, options = options)
+    driver.get(url)
+    time.sleep(2)
+    #Search all the tr tags containings the informations on the actors, the role and the film
+    tr_tags = driver.find_elements(By.TAG_NAME, 'tr')
+    #define a variable actor name to match correctly the film id to the actor in case of several films
+    page_ids = {}
+    actor_name = ""
+
+
+    for tr_tag in tr_tags:
+        # Extract td tag
+        td_tags = tr_tag.find_elements(By.TAG_NAME, 'td')
+    
+        for td_tag in td_tags:
+            #The winners have a golden background on wikipedia so we only consider the one with a golden background
+            style = td_tag.get_attribute('style')    
+            if 'rgb(250, 235, 134)' in style: 
+
+                #Find the i_tag correspond to the film url
+                i_tag = td_tag.find_element(By.TAG_NAME, 'i') if len(td_tag.find_elements(By.TAG_NAME, 'i')) > 0 else None
+                if i_tag:
+                    # Extract the <a> inside <i> in order to get the film url
+                    a_tag = i_tag.find_element(By.TAG_NAME, 'a') if len(i_tag.find_elements(By.TAG_NAME, 'a')) > 0 else None
+                    if a_tag:
+                        film_url = a_tag.get_attribute('href')
+                        #Get the film wikidata id
+                        page_id = get_page_id_from_url(film_url)
+                        if page_id:
+                            page_ids.setdefault(actor_name, []).append(page_id)
+                else:
+                    #If not a film, we want to check if it's an actor
+                    text = td_tag.text.strip()
+                
+                #Separated the cases in case we want to module the code depending on the winning circumstances
+                #Set the variable 'actor_name' to the name of the actor to correctly save the film id with the corresponding actors
+                #All actors won an oscar by playing in a film so each actor is associated with one or several films
+
+                #Actors that accepted the oscar
+                if '‡' in text:
+                    cleaned_text = text.replace('‡', '').strip()
+                    first_two_words = ' '.join(cleaned_text.split()[:2])
+                    actor_name = first_two_words
+
+                #Refused oscar
+                elif '§' in text:
+                    cleaned_text = text.replace('§', '').strip()
+                    first_two_words = ' '.join(cleaned_text.split()[:2])
+                    actor_name = first_two_words
+                
+                #Posthume oscar
+                elif '†' in text:
+                    cleaned_text = text.replace('†', '').strip()
+                    first_two_words = ' '.join(cleaned_text.split()[:2])
+                    actor_name = first_two_words
+                
+    driver.quit()
+    return page_ids
+
+
+def search_winning_films_ids(url, OS):
+    if OS == 'MAC':
+        driver = webdriver.Chrome()
+    else:
+        service = Service('C:\\webdrivers\\chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get(url)
+    time.sleep(5)
+    page_ids = []
+
+    tr_tags = driver.find_elements(By.TAG_NAME, 'tr')
+    for tr_tag in tr_tags:
+
+        #The winners have a golden background on wikipedia so we only consider the one with a golden background
+        style = tr_tag.get_attribute('style')
+        if 'rgb(250, 235, 134)' in style:
+
+            td_tags = tr_tag.find_elements(By.TAG_NAME, 'td')
+            if len(td_tags) > 1:
+                #Get the winning film ID
+                link_tag = td_tags[0].find_element(By.TAG_NAME, 'a')
+                film_url = link_tag.get_attribute('href')
+                
+                # Extract page ID using Wikipedia API
+                page_id = get_page_id_from_url(film_url)
+                if page_id:
+                    page_ids.append(page_id)
+    driver.quit()
+    return page_ids
+
+def get_page_id_from_url(film_url):
+    # Extract the title from the film URL
+    title = film_url.split('/wiki/')[-1]
+    api_url = f"https://en.wikipedia.org/w/api.php?action=query&titles={title}&format=json"
+    
+    #Request to the wikipedia api
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        pages = data.get("query", {}).get("pages", {})
+        #Collect the page ids for the requested urls
+        for page_id, page_data in pages.items():
+            if 'missing' not in page_data:  # Ensures page exists
+                return page_id
+    #If unsucessful return None
+    return None
+
 def load_reviews(movies_list, max_number_of_reviews,OS) :
 
     if OS == 'MAC' :
@@ -164,120 +303,3 @@ def load_reviews(movies_list, max_number_of_reviews,OS) :
                     break
     driver.quit()
     return real_movie_titles, 
-
-
-def search_special_symbols(url, OS) : 
-    if OS == 'MAC' :
-        driver = webdriver.Chrome()
-    else :
-        service = Service('C:\webdrivers\chromedriver.exe')
-        options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome(service = service, options = options)
-    driver.get(url)
-    names_list = []
-    time.sleep(5)
-
-    tr_tags = driver.find_elements(By.TAG_NAME, 'tr')
-
-    for tr_tag in tr_tags:
-        # Extract 'td' elements from the row
-        td_tags = tr_tag.find_elements(By.TAG_NAME, 'td')
-
-        if len(td_tags) > 0:
-            for td_tag in td_tags:
-                text = td_tag.text.strip()
-
-                if '‡' in text:
-                    cleaned_text = text.replace('‡', '').strip()
-                    first_two_words = ' '.join(cleaned_text.split()[:2])
-                    names_list.append(first_two_words)
-
-                #Refused oscar
-                elif '§' in text:
-                    cleaned_text = text.replace('§', '').strip()
-                
-                #Posthume oscar
-                elif '†' in text:
-                    cleaned_text = text.replace('†', '').strip()
-                
-    driver.quit()
-    return names_list
-
-def search_golden_names(url, OS) :
-    if OS == 'MAC' :
-        driver = webdriver.Chrome()
-    else :
-        service = Service('C:\webdrivers\chromedriver.exe')
-        options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome(service = service, options = options)
-
-    driver.get(url)
-    movie_names = []
-    time.sleep(5)
-
-    tr_tags = driver.find_elements(By.TAG_NAME, 'tr')
-
-    for tr_tag in tr_tags:
-        style = tr_tag.get_attribute('style')
-        
-        
-        if 'rgb(250, 235, 134)' in style:  
-        
-            td_tags = tr_tag.find_elements(By.TAG_NAME, 'td')
-            
-            if len(td_tags) > 1:
-                name = td_tags[0].text.strip()
-                movie_names.append(name)
-                movie_info = td_tags[1].text.strip()
-
-
-    driver.quit()
-    movies_oscar_winning = pd.DataFrame(movie_names, columns=['Movie name'])
-
-    return movies_oscar_winning
-
-def search_golden_names_ids(url, OS):
-    if OS == 'MAC':
-        driver = webdriver.Chrome()
-    else:
-        service = Service('C:\\webdrivers\\chromedriver.exe')
-        options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome(service=service, options=options)
-
-    driver.get(url)
-    time.sleep(5)
-    page_ids = []
-
-    tr_tags = driver.find_elements(By.TAG_NAME, 'tr')
-
-    for tr_tag in tr_tags:
-        style = tr_tag.get_attribute('style')
-
-        if 'rgb(250, 235, 134)' in style:
-            td_tags = tr_tag.find_elements(By.TAG_NAME, 'td')
-            if len(td_tags) > 1:
-                link_tag = td_tags[0].find_element(By.TAG_NAME, 'a')
-                film_url = link_tag.get_attribute('href')
-                
-                # Extract page ID using Wikipedia API
-                page_id = get_page_id_from_url(film_url)
-                if page_id:
-                    page_ids.append(page_id)
-
-    driver.quit()
-    movies_oscar_winning = pd.DataFrame(page_ids, columns=['Page ID'])
-    return movies_oscar_winning
-
-def get_page_id_from_url(film_url):
-    # Extract the title from the film URL
-    title = film_url.split('/wiki/')[-1]
-    api_url = f"https://en.wikipedia.org/w/api.php?action=query&titles={title}&format=json"
-    
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        data = response.json()
-        pages = data.get("query", {}).get("pages", {})
-        for page_id, page_data in pages.items():
-            if 'missing' not in page_data:  # Ensures page exists
-                return page_id
-    return None
