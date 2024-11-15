eea93ac5a2963b52e4c8f4e626635d996c7d0547
Wfiles <william.jallot@epfl.ch>
2024-11-15 19:19:17 +0100

    added a docstring to data utils

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 def save_dataframe_to_csv(df, csv_filename) :
-
+    """
+    Save the panda dataframe in the file named data
+    """
     # Get the current directory where the file is located
     current_directory = os.getcwd()
     data_directory =  os.path.abspath(os.path.join(current_directory, 'data'))
@@ -13,6 +15,9 @@ def save_dataframe_to_csv(df, csv_filename) :
     df.to_csv(csv_file_path, index=False)
 
 def load_dataframe_from_csv(file_name) :
+    """
+    Load a panda dataframe in the file named data given file_name
+    """
     # Get the current directory where the file is located
     current_directory = os.getcwd()
     data_directory =  os.path.abspath(os.path.join(current_directory, 'data'))
