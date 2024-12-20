105c8f222ba7e71f7a46ddcf92b6201e70992019
Wfiles <william.jallot@epfl.ch>
2024-12-20 17:23:31 +0100

    continued the website development

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,29 @@ function showContent(sectionId) {
     // Show the selected section
     document.getElementById(sectionId).style.display = 'block';
 }
+
+
+function switchGraph() {
+    // Get the graph container and caption
+    const graphContainer = document.getElementById('graph-container');
+    const graphCaption = document.getElementById('graph-caption');
+
+    // Check the current graph and toggle
+    if (graphContainer.innerHTML.includes('actors_genres_histo.html')) {
+        // Switch to the new graph
+        graphContainer.innerHTML = `
+            {% include plots/genres_cloud.html %}
+            <div id="graph-caption" class="plot-caption">
+                <p>A visualization showing another perspective on genre representation.</p>
+            </div>
+        `;
+    } else {
+        // Switch back to the original graph
+        graphContainer.innerHTML = `
+            {% include plots/actors_genres_histo.html %}
+            <div id="graph-caption" class="plot-caption">
+                <p>A visualization showing the genre breakdown of movies featuring Oscar-winning actors.</p>
+            </div>
+        `;
+    }
+}
